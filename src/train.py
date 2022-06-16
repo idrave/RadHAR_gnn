ba6801f30fa18c2a02b3efd604176b4635695f09
ivan <ivandanielra@gmail.com>
2022-06-16 15:18:28 +0200

    Added command line arguments

--- src/train.py
+++ src/train.py
@@ -3,47 +3,51 @@ import torch.nn as nn
 import os
 from MyDataset import LMDBDataset
 from torch.utils.data import Dataset, DataLoader
-
+import argparse
 from PointGNN import HAR_PointGNN
+from pathlib import Path
+import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #device = torch.device("cpu")
 
 
-def test_acc(model, dataset, batch_size):
-    dataloader = DataLoader(dataset = dataset,batch_size=batch_size,shuffle=False)
-    model.eval()
-
-    test_correct = 0
-    for data in dataloader:
-        #inputs, states, targets = data[0].to(device), data[1].to(device), data[2].to(device)
-        inputs, targets = data[0].to(device), data[1].to(device)
-        frame_sz = data[2].to(device)
-        outputs = model(inputs, frame_sz)
-
-        _, pred = torch.max(outputs, 1)
-        test_correct += torch.sum(pred == targets)
-        # print(test_correct)
-    del dataloader
-    print("Test Accuracy {:.4f}%".format(100.0*test_correct/len(dataset)))
-
-
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data_path', required=True, help="Path to data folder")
+    parser.add_argument('--replace', action='store_true', help="Whether to replace existing LMDB in data directory")
+    parser.add_argument('--num_workers', type=int, default=1, help="Number of workers for DataLoaders")
+    parser.add_argument('--epochs', type=int, default=20, help="Number of epochs to train for")
+    parser.add_argument('--log_steps', type=int, default=300, help="Train logging frequency in number of batches")
+    parser.add_argument('--bs_train', type=int, default=8, help="Training batch size")
+    parser.add_argument('--bs_eval', type=int, default=8, help="Evaluation batch size")
+    parser.add_argument('--lr', type=float, default=1e-4, help="Initial learning rate")
+    # Model args
+    parser.add_argument('-r', type=float, default=0.05, help="Radius for PointGNN adjacency")
+    parser.add_argument('--layers', type=int, default=3, help="Number of PointGNN layers")
+    # Scheduler args
+    parser.add_argument('--steplr_size', type=int, default=1, help="Frequency of scheduler step in epochs")
+    parser.add_argument('--steplr_gamma', type=float, default=0.9, help="Learning rate decay factor")
+    if args:
+        return parser.parse_args(args)
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    batch_size = 2
-    test_batch = 2
-    learning_rate = 0.0001
-
-    epoch_num = 2
-
-    #dataset_test = MyDataset('../Data/lmdbData_test', padding='zero')
-
-    #dataset = MyDataset('../Data/lmdbData_train',padding='zero')
-    dataset = LMDBDataset('data/Train','data/lmdbData_train',60,replace=False)
-    dataset_test = LMDBDataset('data/Test','data/lmdbData_test',60,replace=False)
-    train_loader = DataLoader(dataset = dataset,batch_size=batch_size,shuffle=True)
+    args = parse_args()
+    batch_size = args.bs_train
+    test_batch = args.bs_eval
+    learning_rate = args.lr
+    frame_num = 60
+    epoch_num = args.epochs
+    data_path = Path(args.data_path)
+    num_workers = args.num_workers
+    
+    dataset = LMDBDataset(str(data_path/'Train'),str(data_path/'lmdbData_train'),frame_num,replace=args.replace)
+    dataset_test = LMDBDataset(str(data_path/'Test'),str(data_path/'lmdbData_test'),frame_num,replace=args.replace)
+    train_loader = DataLoader(dataset = dataset,batch_size=batch_size,shuffle=True,num_workers=num_workers)
+    test_loader = DataLoader(dataset = dataset_test,batch_size=test_batch,shuffle=False,num_workers=num_workers)
     print(device)
-    model = HAR_PointGNN(r = 0.05, T=3, state_dim=8)
+    model = HAR_PointGNN(r =args.r, T=args.layers, state_dim=8)
     model.to(device)
 
     # if os.path.exists('./models/HAR_PointGNN.pkl'):
@@ -51,29 +55,47 @@ if __name__ == '__main__':
     #     print("load model sucessfully")
 
     adam = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.StepLR(adam, step_size=1, gamma=0.9)
+    scheduler = torch.optim.lr_scheduler.StepLR(adam, step_size=args.steplr_size, gamma=args.steplr_gamma)
 
     crossloss = nn.CrossEntropyLoss()
     print('Starting training')
-    for epoch in range(1,epoch_num+1):
-        test_acc(model,dataset_test,test_batch)
+    batch_num = 1
+    for epoch in range(epoch_num):
         model.train()
         epoch_loss = 0
-
-        for batch, data in enumerate(train_loader):
-            #inputs, states, targets = data[0].to(device), data[1].to(device), data[2].to(device)
-            inputs, targets = data[0].to(device), data[1].to(device)
-            frame_sz = data[2].to(device)
+        start = time.time()
+        for i, batch in enumerate(train_loader):
+            inputs = batch[0].to(device)
+            targets = batch[1].to(device)
+            frame_sz = batch[2].to(device)
+            adam.zero_grad()
             outputs = model(inputs,frame_sz)
             loss = crossloss(outputs,targets)
-
-            adam.zero_grad()
             loss.backward()
             adam.step()
             epoch_loss += loss
-
-            # print('epoch:{}\t batch:{}/{}\t batch loss:{:.4f}'.format(epoch,batch,len(train_loader),loss))
+            if batch_num % args.log_steps == 0:
+                print('[TRAIN {:0>5d} | {:0>3d}] loss {:.5f}'.format(
+                        i + 1, epoch + 1, loss))
+            batch_num += 1
+            break
         scheduler.step()
-        print('epoch:{}\t epoch loss:{:.4f} \t learning rate:{}'.format(epoch, epoch_loss, adam.param_groups[0]['lr']))
+        print('[TRAIN {}] epoch loss:{:.5f}\t elapsed:{:.2f}'.format(
+                        epoch + 1, epoch_loss, time.time()-start))
+        # Save model
         torch.save(model.state_dict(), "./models/HAR_PointGNN.pkl")
+        # Evaluate model
+        model.eval()
+        test_correct = 0
+        for batch in test_loader:
+            inputs = batch[0].to(device)
+            targets = batch[1].to(device)
+            frame_sz = batch[2].to(device)
+            outputs = model(inputs, frame_sz)
+            _, pred = torch.max(outputs, 1)
+            test_correct += torch.sum(pred == targets)
+            break
+        acc = 100.0*test_correct/len(test_loader)
+        print('[VALID {}] accuracy:{}\t elapsed:{:.2f}'.format(
+                        epoch + 1, acc, time.time()-start))
 
